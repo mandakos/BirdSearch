25b527a0f3077cf3469dcab7101092f305649605
Manda Kosola <t6koma04@students.oamk.fi>
2018-10-16 23:50:39 +0300

    Bird luokka olioiden luomiselle

---
+++ app/src/main/java/com/example/manda/birdsearch/Bird.java
@@ -0,0 +1,29 @@
+package com.example.manda.birdsearch;
+
+import java.io.Serializable;
+
+public class Bird implements Serializable {
+    public String name_finnish; // Lajin suomen kielinen nimi
+    public String name_latin; // Lajin latinan kielinen nimi
+    public String desc; // Lajin kuvaus (species description)
+    public String author; // Kirjoittaja
+
+    public Bird(String name_latin, String desc, String author){
+        this.name_finnish = name_finnish;
+        this.name_latin = name_latin;
+        this.desc = desc;
+        this.author = author;
+    }
+
+    public Bird(String name_latin){
+        this.name_latin = name_latin;
+    }
+
+    public String getName_latin() {
+        return name_latin;
+    }
+
+    public void setName_latin(String name_latin) {
+        this.name_latin = name_latin;
+    }
+}
